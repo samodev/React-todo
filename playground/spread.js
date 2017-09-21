f481de690a5346e612c539f62bde66e83485aac5
stephane <samodev1725@gmail.com>
2017-09-21 21:56:52 +0200

    Add playground for spread operator

---
+++ playground/spread.js
@@ -0,0 +1,17 @@
+// function add (a, b) {
+//   return a+b;
+// };
+//
+// console.log(add(3, 1));
+//
+// var toAdd = [9, 5];
+//
+// console.log(add(...toAdd));
+//
+// var groupA = ['stéphane', 'abdul'];
+//
+// var groupB = ['wesh']
+//
+// var final = [...groupB, 3, ...groupA];
+//
+// console.log(final);
